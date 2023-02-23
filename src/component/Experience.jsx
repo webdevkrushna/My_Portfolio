eb4e0d5051dfa4ff606f97641e45384838de4d98
webdevkrushna <itskrushnakale@gmail.com>
2023-02-23 20:17:39 +0530

    fixed HomePage bug & added onclick in portfolio

--- src/component/Experience.jsx
+++ src/component/Experience.jsx
@@ -57,11 +57,11 @@ const Experience = () => {
     ]
 
   return (
-    <div name="experience" className='bg-gradient-to-b from-gray-800 to-black w-full h-screen'>
+    <div name="experience" className='bg-gradient-to-b from-gray-800 to-black w-full h-full'>
        <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
         <div >
             <p className="text-4xl font-bold border-b-4 border-b-gray-500 p-2 inline">Experience</p>
-            <p className="py-4">These are Technologies I hava worked with</p>
+            <p className="py-6">These are Technologies I hava worked with</p>
         </div>
 
         <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-6 px-12 sm:px-0">
